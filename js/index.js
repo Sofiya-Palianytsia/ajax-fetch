8460fde3ca5116d536e8b3b2d5ff6f91b64fd6bf
Sofiya-Palianytsia <sofiya.palianytsia@gmail.com>
2019-08-10 15:50:36 +0000

    server

--- js/index.js
+++ js/index.js
@@ -60,3 +60,22 @@ function fetchJson() {
         document.querySelector('.account-balance').innerText = clientData.balance;
     });
 }
+
+
+document.querySelector('.login-form input[type=submit]')
+.addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch ('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password').value
+       })
+    })
+    .then( _ => document.querySelector('.login-form').reset());
+}
